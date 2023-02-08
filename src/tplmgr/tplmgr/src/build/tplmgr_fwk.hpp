8296cf133b2f3e35d15138af4cd291cf61920e7a
MateuszJanduraUszu <Mati4321@interia.pl>
2023-02-08 23:29:02 +0100

    Add build files

---
+++ src/tplmgr/tplmgr/src/build/tplmgr_fwk.hpp
@@ -0,0 +1,12 @@
+// tplmgr_fwk.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _TPLMGR_FWK_HPP_
+#define _TPLMGR_FWK_HPP_
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif // _TPLMGR_FWK_HPP_
---
+++ src/tplmgr/tplmgr/src/build/tplmgr_pch.cpp
@@ -0,0 +1,6 @@
+// tplmgr_pch.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <build/tplmgr_pch.hpp>
